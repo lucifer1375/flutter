82a6a4d2bd929c35b564cfcd48b33884dcaf3298
lucifer1375 <mkh938@gmail.com>
2021-10-26 18:06:36 +0330

    cline code part 1 test feild

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,40 @@
-
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() =>runApp(const MaterialApp(
-  home:tas(),
-));
- class tas extends StatelessWidget {
-   const tas({Key? key}) : super(key: key);
- 
-   @override
-   Widget build(BuildContext context) {
-     return MaterialApp(
-       home: Scaffold(
-         backgroundColor:Colors.yellow,
-         appBar: AppBar(
-           centerTitle: true,
-           title: Text ('lucy(1)',style: TextStyle(fontSize: 25.0),),
-           backgroundColor:Colors.teal,
+void main() => runApp(const MaterialApp(
+      home: Tas(),
+    ));
 
-         ),
-         body: Container(
-           color:Colors.redAccent,
-           child: Row(
-             children: [
-               Expanded(child:Image(image:AssetImage('images/dd.jpg')) 
-               ),
-Expanded(child: Image(image:AssetImage('images/dd.jpg'),))
+class Tas extends StatelessWidget {
+  const Tas({Key? key}) : super(key: key);
 
-             ],
-           ),
-         ),
-       ),
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.purple,
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            'lucy(1)',
+            style: TextStyle(fontSize: 25.0),
+          ),
+          backgroundColor: Colors.teal,
+        ),
+        body: Center(
+          child:Row(
+            children: const [
+              Expanded(
 
-     );
-
-   }
- }
- 
+                  child: Image(image: AssetImage('images/dd.jpg'))),
+              Expanded(
+                  child: Image(
+                    image: AssetImage('images/dd.jpg'),
+                  ))
+            ],
+          ) ,
+        ),
+      ),
+    );
+  }
+}
