8634da11009b8b7f17aff3e79be57164c24f262a
lucifer1375 <mkh938@gmail.com>
2021-10-26 15:09:57 +0330

    test1 fild 2

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,28 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() =>runApp(const lucifer());
- class lucifer extends StatelessWidget {
-   const lucifer({Key? key}) : super(key: key);
+void main() =>runApp(const MaterialApp(
+  home:tas(),
+));
+ class tas extends StatelessWidget {
+   const tas({Key? key}) : super(key: key);
  
    @override
    Widget build(BuildContext context) {
      return MaterialApp(
+       home: Scaffold(
+         appBar: AppBar(
+           centerTitle: true,
+           title: Text ('lucy(1)',style: TextStyle(fontSize: 25.0),),
+           backgroundColor:Colors.teal,
 
-
-
-     )
+         ),
+         body: Container(
+           color:Colors.redAccent,
+         ),
+       ),
+     );
+     deb
    }
  }
  
